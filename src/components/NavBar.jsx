2202593ee7324297d1f402812e9e9f03300f3005
sorina tudor <sorinatudor94@gmail.com>
2024-08-08 23:00:20 +0200

    implement searchbar and search results cards

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
-import { Search } from "./";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+import { Search } from "./";
 
 const NavBar = () => {
   return (
@@ -7,11 +8,13 @@ const NavBar = () => {
       <div className="flex bg-amazoneclone text-white h-[60px]">
         {/*Left */}
         <div className="flex items-center m-4">
-          <img
-            className="h-[35px] w-[100px] m-2"
-            src={"../images/amazon.png"}
-            alt=""
-          />
+          <Link to={"./"}>
+            <img
+              className="h-[35px] w-[100px] m-2"
+              src={"../images/amazon.png"}
+              alt=""
+            />
+          </Link>
           <div className="pr-4 pl-4">
             <div className="text-xs xl:text-sm">Deliver to</div>
             <div className="text-sm xl:text-base font-bold">United Kingdom</div>
@@ -33,10 +36,12 @@ const NavBar = () => {
             <div className="text-xs xl:text-sm">Returns</div>
             <div className="text-sm xl:text-base font-bold">& Orders</div>
           </div>
-          <div className="flex pr-3 pl-3">
-            <ShoppingCartIcon className="h-[48px]" />
-            <div className="mt-7 text-xs xl:text-sm font-bold">Cart</div>
-          </div>
+          <Link to={"/checkout"}>
+            <div className="flex pr-3 pl-3">
+              <ShoppingCartIcon className="h-[48px]" />
+              <div className="mt-7 text-xs xl:text-sm font-bold">Cart</div>
+            </div>
+          </Link>
         </div>
       </div>
       <div className="flex text-white bg-amazoneclone-light_blue space-x-3 text-xs xl:text-sm p-2 pl-6">
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,94 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useState, useEffect } from "react";
+import { callAPI } from "../utils/CallApi";
+import { useNavigate, createSearchParams } from "react-router-dom";
+
 const Search = () => {
+  const [suggestions, setSuggestions] = useState(null);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [category, setCategory] = useState("All");
+
+  const navigate = useNavigate();
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate({
+      pathname: "search",
+      search: `${
+        createSearchParams({
+        category: `${category}`,
+        searchTerm: `${searchTerm}`
+        })
+      }`,
+    })
+
+    setSearchTerm("");
+    setCategory("All");
+  };
+
+  const getSuggestions = () => {
+    callAPI(`data/suggestions.json`).then((suggestionsResults) => {
+      setSuggestions(suggestionsResults);
+    });
+  };
+
+  useEffect(() => {
+    getSuggestions();
+  }, []);
+
   return (
     <div className="w-[100%]">
       <div className="flex items-center h-10 bg-amazoneclone-yellow rounded">
-        <select className="p-2 bg-gray-300 text-black border text-xs xl:text-sm rounded">
-          <option value="">All</option>
-          <option value="">Deals</option>
-          <option value="">Amazon</option>
-          <option value="">Fashion</option>
-          <option value="">computers</option>
-          <option value="">Home</option>
-          <option value="">Mobiles</option>
+        <select
+          className="p-2 bg-gray-300 text-black border text-xs xl:text-sm rounded"
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option>All</option>
+          <option>Deals</option>
+          <option>Amazon</option>
+          <option>Fashion</option>
+          <option>Computers</option>
+          <option>Home</option>
+          <option>Mobiles</option>
         </select>
-        <input className="flex items-center grow h-[100%] rounded-l text-black" type="text"/>
-        <button className="w-[45px]">
+
+        <input
+          className="flex items-center grow h-[100%] rounded-l text-black"
+          type="text"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+        />
+
+        <button onClick={onHandleSubmit} className="w-[45px]">
           <MagnifyingGlassIcon className="h-[27px] m-auto stroke-slate-900" />
         </button>
       </div>
+      {suggestions && (
+        <div className="bg-white text-black w-full z-40 absolute">
+          {suggestions
+            .filter((suggestion) => {
+              const currentSearchTerm = searchTerm.toLowerCase();
+              const title = suggestion.title.toLowerCase();
+              return (
+                currentSearchTerm &&
+                title.startsWith(currentSearchTerm) &&
+                title !== currentSearchTerm
+              );
+            })
+            .slice(0, 10)
+            .map((suggestion) => (
+              <div
+                key={suggestion.id}
+                onClick={() => setSearchTerm(suggestion.title)}
+              >
+                {suggestion.title}
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   );
 };
